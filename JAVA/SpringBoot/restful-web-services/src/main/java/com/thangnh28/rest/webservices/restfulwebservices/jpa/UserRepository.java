b9c100f713e7ed8fa538708a6c24af1d00a9b579
Ông Huy Thắng <85379297+hackmehust@users.noreply.github.com>
2024-03-21 18:44:32 +0700

    Delete JAVA directory

--- JAVA/SpringBoot/restful-web-services/src/main/java/com/thangnh28/rest/webservices/restfulwebservices/jpa/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thangnh28.rest.webservices.restfulwebservices.jpa;
-
-import com.thangnh28.rest.webservices.restfulwebservices.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
