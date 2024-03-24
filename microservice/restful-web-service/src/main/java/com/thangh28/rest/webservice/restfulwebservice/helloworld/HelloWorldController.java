3bb610c21dbdbd48135b65be1dc65bc8366e48b6
nhtha <ngohuythang1998@gmail.com>
2024-03-24 19:27:52 +0700

    Đa ngôn ngữ

--- microservice/restful-web-service/src/main/java/com/thangh28/rest/webservice/restfulwebservice/helloworld/HelloWorldController.java
+++ microservice/restful-web-service/src/main/java/com/thangh28/rest/webservice/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.thangh28.rest.webservice.restfulwebservice.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 // REST API
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -21,5 +31,13 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorlPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+        //return "Hello world1 V2";
     }
 }
