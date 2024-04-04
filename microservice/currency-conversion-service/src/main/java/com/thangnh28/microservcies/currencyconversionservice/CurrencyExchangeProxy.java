c5812b7eeaad8e9bc28273a8b60a65b0721d2b36
nhtha <ngohuythang1998@gmail.com>
2024-04-04 17:41:45 +0700

    LoadBalancer

--- microservice/currency-conversion-service/src/main/java/com/thangnh28/microservcies/currencyconversionservice/CurrencyExchangeProxy.java
+++ microservice/currency-conversion-service/src/main/java/com/thangnh28/microservcies/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,11 +4,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000") // name = "currency-exchange-service" khong quan trong
+
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    CurrencyConversion retrieveExchangeValue(
+    public CurrencyConversion retrieveExchangeValue(
             @PathVariable String from,
-            @PathVariable String to
-    );
+            @PathVariable String to);
+
 }
