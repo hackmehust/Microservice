507ecc73068efc83169ff0a4dd264f65281dfdf4
nhtha <ngohuythang1998@gmail.com>
2024-04-09 01:16:00 +0700

    Cấu hình kết nối tới Zipkin

--- microservice/currency-exchange-service/src/main/java/com/thangnh28/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ microservice/currency-exchange-service/src/main/java/com/thangnh28/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,15 +1,17 @@
 package com.thangnh28.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private CurrencyExchangeRepository repository;
@@ -22,6 +24,8 @@ public class CurrencyExchangeController {
             @PathVariable String from,
             @PathVariable String to
     ) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
         if (currencyExchange == null) {
